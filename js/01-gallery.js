7baa34798788ade077e30388b3f061c4e73f7498
taniadavy <tania111davidenko111@gmail.com>
2023-11-05 00:10:40 +0100

    1 and 2 task

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+function createMarkup(arr){
+    const makrkup = arr.map(({preview, original, description}) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" rel="noopener noreferrer">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`);
+    list.insertAdjacentHTML("beforeend", makrkup.join(""));
+};
+
+createMarkup(galleryItems);
+
+list.addEventListener("click", handleClick);
+function handleClick(event){
+    if(event.target === event.currentTarget){
+        return;
+    };
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+instance.show();
+};
+  
---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+function createMarkup(arr){
+    const makrkup = arr.map(({preview, original, description}) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" rel="noopener noreferrer">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`);
+    list.insertAdjacentHTML("beforeend", makrkup.join(""));
+};
+
+createMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250, captionPosition: "bottom"});
+
